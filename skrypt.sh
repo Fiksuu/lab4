61db14efa0f2247cdebc758bd5b1b90c121ec368
Piotr Fiks <piotrfiks560@gmail.com>
2024-07-05 00:05:21 +0200

    Add --logs option to create 100 log files

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,10 @@
 if [[ "$1" == "--date" ]]; then
   date
 fi
+if [[ "$1" == "--logs" ]]; then
+  for i in $(seq 1 100); do
+    echo "log$i.txt" > "log$i.txt"
+    echo "$(basename "$0")" >> "log$i.txt"
+    date >> "log$i.txt"
+  done
+fi
