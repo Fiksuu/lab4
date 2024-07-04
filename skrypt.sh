87b2710888060cf91d37eb92ecbc69da40c16b06
Piotr Fiks <piotrfiks560@gmail.com>
2024-07-05 00:06:36 +0200

    Add --logs <number> option to create a specified number of log files

--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,10 @@ if [[ "$1" == "--logs" ]]; then
     date >> "log$i.txt"
   done
 fi
+if [[ "$1" == "--logs" && "$2" =~ ^[0-9]+$ ]]; then
+  for i in $(seq 1 $2); do
+    echo "log$i.txt" > "log$i.txt"
+    echo "$(basename "$0")" >> "log$i.txt"
+    date >> "log$i.txt"
+  done
+fi
